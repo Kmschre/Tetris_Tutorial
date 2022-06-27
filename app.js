2ec779b210985176ae7bb71cd8a6d176b67db4bc
Kmschre <102772217+Kmschre@users.noreply.github.com>
2022-06-27 12:00:51 -0700

    set drop-down move block left move  block right

--- app.js
+++ app.js
@@ -41,7 +41,12 @@ const iTetromino = [
 const theTetrominoes =[lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
 
 let currentPosition = 4
-let current = theTetrominoes[0][0]
+let currentRotation = 0
+
+//randomly select tetromino and its first rotation
+let random = Math.floor(Math.random()*theTetrominoes.length)
+let current = theTetrominoes[random][currentRotation]
+
 
 //draw the first rotation in the first tetromino
 function draw() {
@@ -50,8 +55,85 @@ function draw() {
     })
 }
 
+draw()
 
-draw();
+//undraw the Tetromino
+function undraw() {
+    current.forEach( index => {
+        squares[currentPosition + index].classList.remove('tetromino')
+    })
+}
+
+//make the tetromino move down every second
+timerId = setInterval(moveDown, 1000)
+
+//assign functions to keyCodes
+function control(e) {
+    if(e.keyCode === 37) {
+        moveLeft()
+    } else if (e.keyCode === 39) {
+        moveRight()
+    } else if (e.keycode === 38) {
+        //rotate
+    } else if (e.keyCode === 40) {
+       // moveDown()
+    }
+}
+document.addEventListener('keyup', control)
+//move down function
+function moveDown() {
+    undraw()
+    currentPosition += width
+    draw()
+    freeze()
+}
+// freeze function 
+function freeze() {
+    if (current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+        current.forEach(index => squares[currentPosition +index].classList.add('taken'))
+
+    //start a new tetromino falling
+
+    random = Math.floor(Math.random() * theTetrominoes.length)
+    current = theTetrominoes[random][currentRotation]
+    currentPosition = 4
+    draw()
+   }
+}
+//move the tetromino left, unless at the edge or is there is blockage
+function moveLeft() {
+    undraw()
+    const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+ 
+    if(!isAtLeftEdge) currentPosition -=1
+
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+        currentPosition +=1
+    }
+
+    draw()
+}
+// move the tetromino right, unless at edge or blocked
+function moveRight() {
+    undraw()
+    const isAtRightEdge = current.some(index => (currentPosition + index) % width === width - 1)
+     
+    if(!isAtRightEdge) currentPosition += 1
+
+    if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+        currentPosition -=1
+    }
+
+    draw()
+}
+//rotate
+
+
+
+
+
+
+
 
 
 })
